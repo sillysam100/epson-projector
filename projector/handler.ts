ff4207684bdc37a2821373367e48c114c3fb6153
Casey Klassen <casey@atom.ca>
2024-07-11 15:44:37 -0600

    Image connection working

--- projector/handler.ts
+++ projector/handler.ts
@@ -6,20 +6,13 @@ import {
   decodeDataBuffer,
 } from "../utils/header";
 
-const udpDiscoverMessageResponse =
+const projectorNameString =
   "01000000454241463637414100000000000000000000000000000000000000000000000001000c00000100020c0000004146363741410000000000000000000000000000000000000000";
-const udpDiscoverMessageResponseBuffer = Buffer.from(
-  udpDiscoverMessageResponse,
-  "hex"
-);
+const projectNameBuffer = Buffer.from(projectorNameString, "hex");
 
-const discoveryMessageString =
-  "01000000454241463637414100000000000000000000000000000000000000000000000001000c00000100020c0000004146363741410000000000000000000000000000000000000000";
-const discoveryMessageBuffer = Buffer.from(discoveryMessageString, "hex");
-
-const dataMessageString =
+const extendedDataString =
   "414636374141000000000000000000000000000000000000000000ba000001040c000000020650423030000003068007b0040000040602008007b00405580709080000000800080080004000e001380400000000070b080000000800080010001000e001380400000000070d080000000800080010001000e0013804000000000000080000000800080010001000e001380400000000070a0100010c022d0000000008089f06f0dd000000000a0a002f0202320401000000000b0b00010a0b09070c02131415160d0a000104401f0000000000000e010005";
-const dataMessageBuffer = Buffer.from(dataMessageString, "hex");
+const extendedDataBuffer = Buffer.from(extendedDataString, "hex");
 
 export function handleMainServerConnection(socket: TCPSocket) {
   console.log("\n\n--Start--");
@@ -27,7 +20,7 @@ export function handleMainServerConnection(socket: TCPSocket) {
     const decoded = decodeDataBuffer(data);
     console.log("Status", decoded.header.status);
     if (decoded.header.status === 2) {
-      const status2Reponse = createDataBuffer(discoveryMessageBuffer, {
+      const status2Reponse = createDataBuffer(projectNameBuffer, {
         controlWord: "EEMP0100",
         ipAddress: decoded.header.ipAddress,
         status: 3,
@@ -36,7 +29,7 @@ export function handleMainServerConnection(socket: TCPSocket) {
       socket.write(status2Reponse);
 
       setTimeout(() => {
-        const status2Reponse = createDataBuffer(dataMessageBuffer, {
+        const status2Reponse = createDataBuffer(extendedDataBuffer, {
           controlWord: "EEMP0100",
           ipAddress: decoded.header.ipAddress,
           status: 21,
@@ -52,13 +45,33 @@ export function handleMainServerConnection(socket: TCPSocket) {
         "ProjectorName",
         decoded.data.subarray(48, 55).toString("ascii")
       );
-      const header = createDataBuffer(discoveryMessageBuffer, {
+      const header = createDataBuffer(projectNameBuffer, {
         controlWord: "EEMP0100",
         ipAddress: decoded.header.ipAddress,
         status: 5,
       });
 
       socket.write(header);
+    } else if (decoded.header.status === 10) {
+      console.log("Header", decoded.header);
+
+      const header = createDataBuffer(Buffer.from(projectNameBuffer), {
+        controlWord: "EEMP0100",
+        ipAddress: decoded.header.ipAddress,
+        status: 5,
+      });
+
+      socket.write(header);
+
+      setTimeout(() => {
+        const header = createDataBuffer(Buffer.from(extendedDataBuffer), {
+          controlWord: "EEMP0100",
+          ipAddress: decoded.header.ipAddress,
+          status: 5,
+        });
+
+        socket.write(header);
+      }, 200);
     }
   });
 
@@ -67,12 +80,22 @@ export function handleMainServerConnection(socket: TCPSocket) {
   });
 }
 export function handleImageServerConnection(socket: TCPSocket) {
+  console.log("\n\n--Start Image Server--");
   socket.on("data", (data) => {
     const decoded = decodeDataBuffer(data);
     console.log("Image Server Connection", decoded);
+    const header = createDataBuffer(projectNameBuffer, {
+      controlWord: "EPRD0600",
+      ipAddress: decoded.header.ipAddress,
+      status: 0,
+    });
+
+    socket.write(header);
   });
 
-  socket.on("close", () => {});
+  socket.on("close", () => {
+    console.log("--End Image Server--");
+  });
 }
 
 export function handleDiscoveryServerConnection(
@@ -88,16 +111,13 @@ export function handleDiscoveryServerConnection(
   const decodedData = decodeDataBuffer(msg);
 
   if (decodedData.header.status === 1) {
-    const discoveryResponse = createDataBuffer(
-      udpDiscoverMessageResponseBuffer,
-      {
-        controlWord: "EEMP0100",
-        ipAddress: decodedData.header.ipAddress,
-        status: 3,
-      }
-    );
+    const discoveryResponse = createDataBuffer(projectNameBuffer, {
+      controlWord: "EEMP0100",
+      ipAddress: decodedData.header.ipAddress,
+      status: 3,
+    });
 
-    const dataResponse = createDataBuffer(dataMessageBuffer, {
+    const dataResponse = createDataBuffer(extendedDataBuffer, {
       controlWord: "EEMP0100",
       ipAddress: decodedData.header.ipAddress,
       status: 21,
